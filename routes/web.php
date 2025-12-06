17d129c9c6d6a8d42004ea3a8e2410c31869400d
Alex Guiu Azuara <alexhey744@gmail.com>
2025-12-06 19:35:15 +0100

    Change Google Auth callback route to root

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ Route::delete('etiquetas/{id_etiqueta}', [EtiquetaController::class, 'destroy'])
 });
 
 // Google Auth
-Route::get('/auth/google/callback', function () {
+Route::get('/', function () {
     $user_google = Socialite::driver('google')->user();
 
     $user = User::updateOrCreate([
